cb3c9f04ce305fde2c0e600df9ed91cb48d93c64
Black <82063930+Blackkk0@users.noreply.github.com>
2021-06-28 17:31:08 +0300

    Update morning.js

--- src/bot/commands/morning.js
+++ src/bot/commands/morning.js
@@ -4,17 +4,16 @@ const azkarM = require("../../db/list/morningAzkar");
 module.exports = {
   name: "morning",
   cooldown: 6,
-  aliases: ["azkar-morning", "اذكار الصباح", "الصباح"],
   run: async (client, message) => {
     var result = azkarM[Math.floor(Math.random() * azkarM.length)];
     message.channel.send(
       new MessageEmbed()
         .setThumbnail("https://athkarapp.com/images/athkarLogo.png")
         .setAuthor(
-          "أذكار الصباح",
+          "Azkar morning",
           "https://athkarapp.com/images/athkarLogo.png"
         )
-        .setTitle("**المصدر**")
+        .setTitle("**source**")
         .setURL(
           "https://www.islambook.com/azkar/1/%D8%A3%D8%B0%D9%83%D8%A7%D8%B1-%D8%A7%D9%84%D8%B5%D8%A8%D8%A7%D8%AD"
         )
