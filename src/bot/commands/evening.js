f353e8119017f47423ddd32fca12c70173c5bcee
Black <82063930+Blackkk0@users.noreply.github.com>
2021-06-28 17:19:36 +0300

    Update evening.js

--- src/bot/commands/evening.js
+++ src/bot/commands/evening.js
@@ -4,7 +4,6 @@ const azkarE = require("../../db/list/eveningAzkar");
 module.exports = {
   name: "evening",
   cooldown: 6,
-  aliases: ["azkar-evening", "اذكار المساء", "المساء"],
   run: async (client, message) => {
     var result = azkarE[Math.floor(Math.random() * azkarE.length)];
     message.channel.send(
@@ -13,10 +12,10 @@ module.exports = {
           "https://athkarapp.com/images/athkarLogo.png"
         )
         .setAuthor(
-          "أذكار المساء",
+          "The evening's remembrance.",
           "https://athkarapp.com/images/athkarLogo.png"
         )
-        .setTitle("**المصدر**")
+        .setTitle("**source**")
         .setURL("https://www.islambook.com/azkar/2/%D8%A3%D8%B0%D9%83%D8%A7%D8%B1-%D8%A7%D9%84%D9%85%D8%B3%D8%A7%D8%A1")
         .setColor(0x2f3136)
         .setDescription("**" + result + "**")
